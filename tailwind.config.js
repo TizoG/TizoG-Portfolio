337d67b3ab43a0494ec67517344b1b194f492564
TizoG <tirado114@hotmail.com>
2026-02-07 17:42:26 +0100

    feat: Initialize portfolio site with core Astro components, project data, and Tailwind CSS styling.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                montserrat: ["Montserrat", "sans-serif"],
+                nunito: ["Nunito", "sans-serif"],
+                jetbrains: ["JetBrains Mono", "monospace"],
+            },
+        },
+    },
+};
